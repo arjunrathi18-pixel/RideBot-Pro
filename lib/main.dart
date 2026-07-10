a9027f232ff26dc2af1151c6e774d3bf18895f34
arjunrathi18-pixel <arjunrathi18@gmail.com>
2026-07-10 23:10:05 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'core/theme/app_theme.dart';
 import 'screens/main_navigation.dart';
 import 'services/app_state_provider.dart';
-
+import 'data/database/ride_database.dart';
+import 'models/ride_model.dart';
+import 'models/settings_model.dart';
 
 
 Future<void> main() async {
@@ -18,6 +20,17 @@ Future<void> main() async {
   // Initialize Local Database
 
   await Hive.initFlutter();
+  Hive.registerAdapter(
+RideModelAdapter()
+);
+
+
+Hive.registerAdapter(
+SettingsModelAdapter()
+);
+
+
+await RideDatabase.init();
 
 
 
