ffc92cff0b6bc932be0398e0bf6e6c32268755bf
arjunrathi18-pixel <arjunrathi18@gmail.com>
2026-07-10 23:01:42 +0530

    Create main_navigation.dart

---
+++ lib/screens/main_navigation.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+import 'dashboard_screen.dart';
+import 'automation_screen.dart';
+import 'settings_screen.dart';
+
+
+
+class MainNavigation extends StatefulWidget {
+
+
+const MainNavigation({
+super.key
+});
+
+
+
+@override
+State<MainNavigation> createState()
+=> _MainNavigationState();
+
+
+}
+
+
+
+
+class _MainNavigationState 
+extends State<MainNavigation>{
+
+
+
+int currentIndex = 0;
+
+
+
+final pages = const [
+
+
+DashboardScreen(),
+
+
+AutomationScreen(),
+
+
+SettingsScreen(),
+
+
+];
+
+
+
+
+
+@override
+Widget build(BuildContext context){
+
+
+return Scaffold(
+
+
+body: pages[currentIndex],
+
+
+
+
+bottomNavigationBar: NavigationBar(
+
+
+selectedIndex: currentIndex,
+
+
+onDestinationSelected:(index){
+
+
+setState((){
+
+
+currentIndex=index;
+
+
+});
+
+
+},
+
+
+
+destinations: const [
+
+
+NavigationDestination(
+
+icon: Icon(Icons.dashboard),
+
+label:"Dashboard"
+
+),
+
+
+
+NavigationDestination(
+
+icon: Icon(Icons.smart_toy),
+
+label:"Automation"
+
+),
+
+
+
+NavigationDestination(
+
+icon: Icon(Icons.settings),
+
+label:"Settings"
+
+),
+
+
+],
+
+
+
+),
+
+
+
+);
+
+
+}
+
+
+}
