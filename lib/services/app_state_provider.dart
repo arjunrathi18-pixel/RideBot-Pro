e40a8321edefeb20c9934986391cbcb0391f0d30
arjunrathi18-pixel <arjunrathi18@gmail.com>
2026-07-11 07:22:37 +0530

    Update app_state_provider.dart

--- lib/services/app_state_provider.dart
+++ lib/services/app_state_provider.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
+
 import '../models/settings_model.dart';
+
+
+import '../data/database/settings_database.dart';
+
+
 
 
 
@@ -8,16 +14,52 @@ class AppStateProvider extends ChangeNotifier {
 
 
 
-bool _automationRunning=false;
+bool _automationRunning = false;
+
 
 
 bool get automationRunning =>
+
 _automationRunning;
 
 
 
-SettingsModel settings =
+
+
+
+SettingsModel _settings =
+
 SettingsModel();
+
+
+
+SettingsModel get settings =>
+
+_ settings;
+
+
+
+
+
+
+Future<void> initialize() async {
+
+
+
+_settings =
+
+SettingsDatabase.getSettings();
+
+
+
+notifyListeners();
+
+
+
+}
+
+
+
 
 
 
@@ -26,32 +68,21 @@ SettingsModel();
 void toggleAutomation(){
 
 
+
 _automationRunning =
+
 !_automationRunning;
 
 
+
 notifyListeners();
+
 
 
 }
 
 
 
-
-
-void updateSettings(
-SettingsModel newSettings
-){
-
-
-settings =
-newSettings;
-
-
-notifyListeners();
-
-
-}
 
 
 
@@ -59,9 +90,14 @@ notifyListeners();
 
 void startAutomation(){
 
-_automationRunning=true;
+
+
+_automationRunning = true;
+
 
 notifyListeners();
+
+
 
 }
 
@@ -69,11 +105,220 @@ notifyListeners();
 
 
 
+
+
+
 void stopAutomation(){
 
-_automationRunning=false;
+
+
+_automationRunning = false;
+
 
 notifyListeners();
+
+
+
+}
+
+
+
+
+
+
+
+
+Future<void> updateSettings(
+
+SettingsModel newSettings
+
+) async {
+
+
+
+_settings = newSettings;
+
+
+
+await SettingsDatabase.saveSettings(
+
+_settings
+
+);
+
+
+
+notifyListeners();
+
+
+
+}
+
+
+
+
+
+
+
+
+
+void updateAutoAccept(
+
+bool value
+
+){
+
+
+
+_settings.autoAccept = value;
+
+
+
+notifyListeners();
+
+
+
+save();
+
+
+
+}
+
+
+
+
+
+
+
+
+void updateMinimumFare(
+
+double value
+
+){
+
+
+
+_settings.minimumFare = value;
+
+
+
+notifyListeners();
+
+
+
+save();
+
+
+
+}
+
+
+
+
+
+
+
+
+void updateMinimumPerKm(
+
+double value
+
+){
+
+
+
+_settings.minimumPerKm = value;
+
+
+
+notifyListeners();
+
+
+
+save();
+
+
+
+}
+
+
+
+
+
+
+
+
+void updateMaximumDistance(
+
+double value
+
+){
+
+
+
+_settings.maximumDistance = value;
+
+
+
+notifyListeners();
+
+
+
+save();
+
+
+
+}
+
+
+
+
+
+
+
+
+void updateAcceptDelay(
+
+int value
+
+){
+
+
+
+_settings.acceptDelay = value;
+
+
+
+notifyListeners();
+
+
+
+save();
+
+
+
+}
+
+
+
+
+
+
+
+
+
+Future<void> save() async {
+
+
+
+await SettingsDatabase.saveSettings(
+
+_settings
+
+);
+
+
 
 }
 
