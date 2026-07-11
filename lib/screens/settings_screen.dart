5c92f4b86a59dac6258234d3b1a629d6c334bb9c
arjunrathi18-pixel <arjunrathi18@gmail.com>
2026-07-11 07:24:52 +0530

    Update settings_screen.dart

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+
 import '../services/app_state_provider.dart';
 
 
@@ -14,11 +15,13 @@ super.key
 
 
 
+
 @override
 Widget build(BuildContext context){
 
 
 final app =
+
 context.watch<AppStateProvider>();
 
 
@@ -26,14 +29,22 @@ context.watch<AppStateProvider>();
 return Scaffold(
 
 
-appBar:
 
-AppBar(
+appBar: AppBar(
 
 title:
-const Text("Settings")
+
+const Text(
+
+"Settings"
 
 ),
+
+centerTitle: true,
+
+),
+
+
 
 
 
@@ -42,96 +53,259 @@ body:
 ListView(
 
 padding:
+
 const EdgeInsets.all(16),
 
 
-children:[
+
+children: [
 
 
+
+
+
+Card(
+
+child:
 
 SwitchListTile(
 
 title:
+
 const Text(
-"Auto Accept"
+
+"Auto Accept Ride"
+
 ),
 
 
+subtitle:
+
+const Text(
+
+"Automatically accept filtered rides"
+
+),
+
+
+
 value:
+
 app.settings.autoAccept,
 
 
-onChanged:(value){
+
+onChanged:
+
+(value){
 
 
+app.updateAutoAccept(
 
-app.settings.autoAccept =
-value;
+value
 
+);
 
-app.notifyListeners();
 
 
 },
 
+
+),
+
+
 ),
 
 
 
 
+
+const SizedBox(
+
+height: 12
+
+),
+
+
+
+
+
+
+Card(
+
+child:
+
 ListTile(
 
 title:
+
 const Text(
+
 "Minimum Fare"
+
 ),
+
 
 
 subtitle:
+
 Text(
-"₹${app.settings.minimumFare}"
+
+"₹ ${app.settings.minimumFare}"
+
+),
+
+
+
+trailing:
+
+const Icon(
+
+Icons.currency_rupee
+
 ),
 
 
 ),
 
 
+),
 
+
+
+
+
+
+Card(
+
+child:
 
 ListTile(
 
 title:
+
 const Text(
-"Minimum ₹/KM"
+
+"Minimum Earning / KM"
+
 ),
+
 
 
 subtitle:
+
 Text(
-"${app.settings.minimumPerKm}"
+
+"₹ ${app.settings.minimumPerKm} / KM"
+
+),
+
+
+
+trailing:
+
+const Icon(
+
+Icons.route
+
 ),
 
 
 ),
 
 
+),
 
+
+
+
+
+Card(
+
+child:
 
 ListTile(
 
 title:
+
 const Text(
+
 "Maximum Distance"
+
 ),
+
 
 
 subtitle:
+
 Text(
+
 "${app.settings.maximumDistance} KM"
+
+),
+
+
+
+trailing:
+
+const Icon(
+
+Icons.location_on
+
 ),
 
 
 ),
+
+
+),
+
+
+
+
+
+
+Card(
+
+child:
+
+ListTile(
+
+title:
+
+const Text(
+
+"Accept Delay"
+
+),
+
+
+
+subtitle:
+
+Text(
+
+"${app.settings.acceptDelay} Seconds"
+
+),
+
+
+
+trailing:
+
+const Icon(
+
+Icons.timer
+
+),
+
+
+),
+
+
+),
+
+
 
 
 
@@ -141,7 +315,9 @@ Text(
 ),
 
 
+
 );
+
 
 
 }
