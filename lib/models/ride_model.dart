667c1e70df8fbbfdbd882b1789d6242c6151b842
arjunrathi18-pixel <arjunrathi18@gmail.com>
2026-07-11 07:06:29 +0530

    Update ride_model.dart

--- lib/models/ride_model.dart
+++ lib/models/ride_model.dart
@@ -1,7 +1,10 @@
 import 'package:hive/hive.dart';
 
 
+
 part 'ride_model.g.dart';
+
+
 
 
 
@@ -11,59 +14,220 @@ class RideModel extends HiveObject {
 
 
 
-@HiveField(0)
-String platform;
-
-
-@HiveField(1)
-double fare;
-
-
-@HiveField(2)
-double distance;
-
-
-@HiveField(3)
-double earningPerKm;
-
-
-@HiveField(4)
-String pickup;
-
-
-@HiveField(5)
-String dropLocation;
-
-
-@HiveField(6)
-String status;
-
-
-@HiveField(7)
-DateTime createdAt;
+  @HiveField(0)
+  final String platform;
 
 
 
-RideModel({
-
-required this.platform,
-
-required this.fare,
-
-required this.distance,
-
-required this.earningPerKm,
-
-required this.pickup,
-
-required this.dropLocation,
-
-required this.status,
-
-required this.createdAt,
+  @HiveField(1)
+  final double fare;
 
 
-});
+
+  @HiveField(2)
+  final double distance;
+
+
+
+  @HiveField(3)
+  final double earningPerKm;
+
+
+
+  @HiveField(4)
+  final String pickup;
+
+
+
+  @HiveField(5)
+  final String dropLocation;
+
+
+
+  @HiveField(6)
+  final String status;
+
+
+
+  @HiveField(7)
+  final DateTime createdAt;
+
+
+
+
+
+  RideModel({
+
+
+    required this.platform,
+
+
+    required this.fare,
+
+
+    required this.distance,
+
+
+    required this.earningPerKm,
+
+
+    required this.pickup,
+
+
+    required this.dropLocation,
+
+
+    required this.status,
+
+
+    required this.createdAt,
+
+
+  });
+
+
+
+
+
+
+
+
+  RideModel copyWith({
+
+
+    String? platform,
+
+
+    double? fare,
+
+
+    double? distance,
+
+
+    double? earningPerKm,
+
+
+    String? pickup,
+
+
+    String? dropLocation,
+
+
+    String? status,
+
+
+    DateTime? createdAt,
+
+
+  }) {
+
+
+
+    return RideModel(
+
+
+
+      platform:
+
+          platform ?? this.platform,
+
+
+
+      fare:
+
+          fare ?? this.fare,
+
+
+
+      distance:
+
+          distance ?? this.distance,
+
+
+
+      earningPerKm:
+
+          earningPerKm ?? this.earningPerKm,
+
+
+
+      pickup:
+
+          pickup ?? this.pickup,
+
+
+
+      dropLocation:
+
+          dropLocation ?? this.dropLocation,
+
+
+
+      status:
+
+          status ?? this.status,
+
+
+
+      createdAt:
+
+          createdAt ?? this.createdAt,
+
+
+
+    );
+
+
+
+  }
+
+
+
+
+
+
+
+  Map<String,dynamic> toMap(){
+
+
+
+    return {
+
+
+
+      "platform": platform,
+
+
+      "fare": fare,
+
+
+      "distance": distance,
+
+
+      "earningPerKm": earningPerKm,
+
+
+      "pickup": pickup,
+
+
+      "dropLocation": dropLocation,
+
+
+      "status": status,
+
+
+      "createdAt": createdAt.toIso8601String(),
+
+
+
+    };
+
+
+  }
+
+
+
+
 
 
 
